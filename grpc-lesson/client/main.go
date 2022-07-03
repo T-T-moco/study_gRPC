818810755237ae6fa0e59d893adc3cecc2891bc2
武政智子 <tomoko.takemasa.ow@gmail.com>
2022-07-03 17:16:23 +0900

    [update] クライアントストリーミングRPCのクライアント側実装

--- grpc-lesson/client/main.go
+++ grpc-lesson/client/main.go
@@ -6,6 +6,8 @@ import (
 	"grpc-lesson/pb"
 	"io"
 	"log"
+	"os"
+	"time"
 
 	"google.golang.org/grpc"
 )
@@ -18,8 +20,9 @@ func main() {
 	defer conn.Close()
 
 	client := pb.NewFileServiceClient(conn)
-	// callListFiles(client)
-	callDownload(client)
+	// callListFiles(client) // 普通の関数を呼び出す
+	// callDownload(client) // メイン関数からサーバーストリーミングRPCを呼び出す
+	CallUpload(client) // メイン関数からクライアントストリーミングRPCを呼び出す
 }
 
 func callListFiles(client pb.FileServiceClient) {
@@ -31,6 +34,12 @@ func callListFiles(client pb.FileServiceClient) {
 	fmt.Println(res.GetFilenames())
 }
 
+/*****************************************************
+* サーバーストリーミングRPC（１リクエストに複数レスポンス）
+******************************************************/
+// １つのリクエストを渡した後複数のレスポンスが帰ってくる。
+// 1. クライアントからファイルをアップロード
+// 2. バックエンドからファイルの中身を5バイトずつクライアントに返す
 func callDownload(client pb.FileServiceClient) {
 	req := &pb.DownloadRequest{Filename: "name.txt"}
 	stream, err := client.Download(context.Background(), req)
@@ -50,4 +59,52 @@ func callDownload(client pb.FileServiceClient) {
 		log.Printf("Response from Download(bytes): %v", res.GetData())
 		log.Printf("Response from Download(string): %v", string(res.GetData()))
 	}
+}
+
+/*****************************************************
+* クライアントストリーミングRPC（複数リクエストに１レスポンス）
+******************************************************/
+// 複数のリクエストを渡し、渡し終わってからレスポンスを受け取る
+// 1. 5バイトずつリクエストを送り、バックエンドで都度表示している。
+// 2. 全てリクエストで送り終わってから、バックエンドから総サイズ数をクライアントに返す
+func CallUpload(client pb.FileServiceClient) {
+	filename := "sports.txt"
+	path := "/Users/takemasatomoko/Desktop/source/practice/gRPC/grpc-lesson/strage/" + filename // クライアントのストレージであると想定
+
+	file, err := os.Open(path) // ファイルを開く
+	if err != nil {            // ファイルを開けなかったら
+		log.Fatalln(err)
+	}
+	defer file.Close() // ファイルをクローズする
+
+	stream, err := client.Upload(context.Background()) // ストリームが取得できる
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	buf := make([]byte, 5) // データ格納用のバッファを用意する
+	for {
+		n, err := file.Read(buf)     // ファイルの内容をバッファに格納し
+		if n == 0 || err == io.EOF { // この場合はループを抜ける
+			break
+		}
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		req := &pb.UploadRequest{Data: buf[:n]} // 読み込んだ内容をリクエストに詰める
+		sendErr := stream.Send(req)             // ストリームからリクエストを送信することができる
+		if sendErr != nil {
+			log.Fatalln(sendErr)
+		}
+
+		time.Sleep(1 * time.Second) // 1秒間スリープを入れる
+	}
+
+	res, err := stream.CloseAndRecv() // リクエストの終了をサーバーに通知し、レスポンスを受け取ることができる
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Printf("received data size: %v", res.GetSize())
 }
